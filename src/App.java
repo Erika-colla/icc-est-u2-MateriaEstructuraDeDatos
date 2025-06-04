f34eb460a5cf80a908041fa07146a442288b4261
Erika-colla <collaguazobelen8@gmail.com>
2025-06-04 01:45:00 -0500

    materia estructura

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import controllers.Stack;
+import models.Node;
+
+public class App {
+    Stack stack = new Stack();
+    stack.push(10);
+    stack.push(-1);
+    stack.push(0);
+    stack.push(5);
+    stack.printStack();
+    stack.pop();
+    stack.peek();
+    stack.printStack;
+
+}
